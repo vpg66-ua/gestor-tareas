78b3f772898b98ebc419672ffad27b834f5bddb1
Victor Padron <vpg66@gcloud.ua.es>
2024-10-27 11:14:05 +0100

    Deleted example code and added version and Enviroment of the project.

--- GestorTareas/Program.cs
+++ GestorTareas/Program.cs
@@ -8,6 +8,7 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+// It's important to see the different HTTP methods that are available in the Swagger UI
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -16,29 +17,4 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
-
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
